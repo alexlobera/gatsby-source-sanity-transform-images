d6d44e97fc03791aa84471fb8da50a8a6d88302e
alexlbr <alex.lobera@gmail.com>
2019-09-13 21:12:00 +0200

    refactores folder structure and updates .gitignore

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,70 @@
-const { GraphQLInt, GraphQLString } = require('gatsby/graphql')
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+"use strict";
 
-exports.setFieldsOnGraphQLNodeType = ({ type }) => {
-  if (type.name === `SanityImageAsset`) {
+var _require = require('gatsby/graphql'),
+    GraphQLInt = _require.GraphQLInt,
+    GraphQLString = _require.GraphQLString;
+
+var _require2 = require("gatsby-source-filesystem"),
+    createRemoteFileNode = _require2.createRemoteFileNode;
+
+exports.setFieldsOnGraphQLNodeType = function (_ref) {
+  var type = _ref.type;
+
+  if (type.name === "SanityImageAsset") {
     return {
       localFile: {
-        type: `File`,
+        type: "File",
         args: {
           width: {
             type: GraphQLInt,
-            defaultValue: 600,
+            defaultValue: 600
           },
           format: {
             type: GraphQLString,
-            defaultValue: 'jpg',
+            defaultValue: 'jpg'
           },
           height: {
-            type: GraphQLInt,
+            type: GraphQLInt
           },
           fit: {
             type: GraphQLString,
-            defaultValue: 'crop',
-          },
-        },
-      },
-    }
-  }
+            defaultValue: 'crop'
+          }
+        }
+      }
+    };
+  } // by default return empty object
 
-  // by default return empty object
-  return {}
-}
 
-exports.createResolvers = ({
-  actions: { createNode },
-  cache,
-  createNodeId,
-  createResolvers,
-  store,
-  reporter,
-}) => {
-  const resolvers = {
+  return {};
+};
+
+exports.createResolvers = function (_ref2) {
+  var createNode = _ref2.actions.createNode,
+      cache = _ref2.cache,
+      createNodeId = _ref2.createNodeId,
+      createResolvers = _ref2.createResolvers,
+      store = _ref2.store,
+      reporter = _ref2.reporter;
+  var resolvers = {
     SanityImageAsset: {
       localFile: {
-        resolve: (source, { width, height, fit, format }) => {
+        resolve: function resolve(source, _ref3) {
+          var width = _ref3.width,
+              height = _ref3.height,
+              fit = _ref3.fit,
+              format = _ref3.format;
           return createRemoteFileNode({
-            url: `${source.url}?w=${width}&fm=${format}${
-              height ? `&h=${height}` : ''
-            }&fit=${fit}`,
-            store,
-            cache,
-            createNode,
-            createNodeId,
-            reporter,
-          })
-        },
-      },
-    },
-  }
-  createResolvers(resolvers)
-}
+            url: "".concat(source.url, "?w=").concat(width, "&fm=").concat(format).concat(height ? "&h=".concat(height) : '', "&fit=").concat(fit),
+            store: store,
+            cache: cache,
+            createNode: createNode,
+            createNodeId: createNodeId,
+            reporter: reporter
+          });
+        }
+      }
+    }
+  };
+  createResolvers(resolvers);
+};
